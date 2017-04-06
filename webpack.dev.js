49f1377eae5366623a23b8b8d12d05632f56f16e
baoxd <1459815090@qq.com>
2017-04-06 21:56:24 +0800

    add webpack

---
+++ webpack.dev.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const cleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: ["./app/src/index"],
+		vendor: ['react']
+	},
+	output: {
+		path: path.join(__dirname, "app/dist"),
+		filename: "[name].js",
+		publicPath: "http://localhost:8080",
+		chunkFilename: "[name].js"
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx', '.less', '.scss', '.css'],
+		modules: ['node_modules', './app/src']
+	},
+	module: {
+		loaders: [{
+			test: /\.js[x]?$/,
+			include: path.join(__dirname, "app/src"),
+			exclude: /node_modules/,
+			loader: "babel-loader"
+		}]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('development')
+			},
+			BASE_URL: JSON.stringify('http://localhost:8001'),
+		}),
+		// webpack 2 不再支持
+		// new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new cleanPlugin([path.join(__dirname, "app/dist")]),
+		new webpack.optimize.CommonsChunkPlugin({
+			names: ['vendor'],
+			filename: 'vendor.js'
+		})
+	],
+	devServer: {
+		port: 8080,
+		contentBase: path.join(__dirname, "app/dist"),
+		hot: true,
+		historyApiFallback: true,
+		publicPath: "",
+		stats: {
+			colors: true
+		}
+	}
+}
